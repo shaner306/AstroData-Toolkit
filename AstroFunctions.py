96d8a315703f66a72155068fe9d0c91a4760ad59
Jack Wawrow <46384597+jmwawrow@users.noreply.github.com>
2021-04-15 15:30:17 -0400

    Update AstroFunctions.py

--- AstroFunctions.py
+++ AstroFunctions.py
@@ -9,6 +9,8 @@ Created on Thu Apr 15 10:14:43 2021
 @author: Jack Wawrow
 """
 from astropy.io import fits, ascii
+from astropy.stats import sigma_clipped_stats
+from photutils.detection import IRAFStarFinder
 import numpy as np
 
 
@@ -23,7 +25,7 @@ def read_ref_stars(ref_stars_file):
             
     Returns
     -------
-        reference_stars : <astropy.table> object
+        reference_stars : astropy.table
             Table with the data extracted from ref_stars_file.
     """
     try:
@@ -45,7 +47,7 @@ def read_fits_file(filepath):
 
     Returns
     -------
-    hdr : <astropy.io.fits.header.Header> object
+    hdr : astropy.io.fits.header.Header
         Header from the fits file.
     imgdata : numpy.ndarray
         Data from the fits file.
@@ -55,4 +57,44 @@ def read_fits_file(filepath):
         hdr = hdul[0].header
         imgdata = hdul[0].data
     return hdr, imgdata
+
+def detecting_stars(imgdata, fwhm=2.0, bkg=None, bkg_std=None):
+    """
+    Detect stars using IRAFStarFinder.
+
+    Parameters
+    ----------
+    imgdata : numpy.ndarray
+        Data from the fits file.
+    fwhm : float, optional
+        Initial guess of FWHM of the image stars in pixels. The default is 2.0.
+    bkg : float, optional
+        Background value of the image in ADU. The default is None.
+    bkg_std : float, optional
+        Standard deviation of the image background in ADU. The default is None.
+
+    Returns
+    -------
+    irafsources : astropy.table
+        Table containing information of all stars detected in the image.
+        Has columns:
+            id,
+            xcentroid,
+            ycentroid,
+            fwhm,
+            sharpness,
+            roundness,
+            pa,
+            npix,
+            sky,
+            peak,
+            flux,
+            mag
+
+    """
+    if not (bkg or bkg_std):
+        _, bkg, bkg_std = sigma_clipped_stats(imgdata)
+    iraffind = IRAFStarFinder(threshold=bkg+3*bkg_std, fwhm=fwhm)
+    irafsources = iraffind(imgdata - bkg)
+    return irafsources
 
