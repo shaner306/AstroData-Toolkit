0325e2de5a087e85bef6a94ad6a17c7229957308
Jack Wawrow <46384597+jmwawrow@users.noreply.github.com>
2021-04-15 10:17:01 -0400

    Create AstroFunctions.py

---
+++ AstroFunctions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+AstroFunctions.py
+
+This file holds all of the functions that will likely be implemented in a final 
+version of the image processor.
+
+Created on Thu Apr 15 10:14:43 2021
+
+@author: Jack Wawrow
+"""
+
