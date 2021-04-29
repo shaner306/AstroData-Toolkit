b87bc2ca567844b3ee57dc4e33e5045f776021bc
Jack Wawrow <46384597+jmwawrow@users.noreply.github.com>
2021-04-29 11:07:52 -0400

    Update Functiontester.py

--- Functiontester.py
+++ Functiontester.py
@@ -16,7 +16,7 @@ import os
 # ref_stars_file = r'C:\Users\jmwawrow\Documents\DRDC_Code\FITS Tutorial\Reference_stars_mod.csv'
 # directory = r'C:\Users\jmwawrow\Documents\DRDC_Code\2021-03-20 - Calibrated\Solved Images\HIP 2894'
 
-ref_stars_file = r'C:\Users\jmwawrow\Documents\DRDC_Code\FITS Tutorial\Reference_stars_new.txt'
+ref_stars_file = r'C:\Users\jmwawrow\Documents\DRDC_Code\FITS Tutorial\Reference_stars_Apr29.txt'
 directory = r'C:\Users\jmwawrow\Documents\DRDC_Code\2021-04-21\Solved Stars'
 ground_based = True
 
@@ -33,7 +33,7 @@ for dirpath, dirnames, filenames in os.walk(directory):
         if filename.endswith(".fits"):
         # if filename.endswith("_clean.fits"):
             filepath = os.path.join(dirpath, filename)
-            # print(filepath)
+            print(filepath)
             hdr, imgdata = astro.read_fits_file(filepath)
             exptime = hdr['EXPTIME']
             # exptime = hdr['AEXPTIME']
@@ -63,13 +63,28 @@ for dirpath, dirnames, filenames in os.walk(directory):
                 continue
             
             instr_filter = astro.get_instr_filter_name(hdr)
+            _, _, _, colour_index = astro.get_app_mag_and_index(matched_stars.ref_star, instr_filter)
+            field = astro.get_field_name(matched_stars, name_key='Name')
+            if np.isnan(matched_stars.ref_star[colour_index]).any():
+                no_nan_indices = np.invert(np.isnan(matched_stars.ref_star[colour_index]))
+                matched_stars = matched_stars._replace(
+                    ref_star_index = matched_stars.ref_star_index[no_nan_indices],
+                    img_star_index = matched_stars.img_star_index[no_nan_indices],
+                    ref_star = matched_stars.ref_star[no_nan_indices],
+                    ref_star_loc = matched_stars.ref_star_loc[no_nan_indices],
+                    img_star_loc = matched_stars.img_star_loc[no_nan_indices],
+                    ang_separation = matched_stars.ang_separation[no_nan_indices],
+                    img_instr_mag = matched_stars.img_instr_mag[no_nan_indices],
+                    img_instr_mag_sigma = matched_stars.img_instr_mag_sigma[no_nan_indices],
+                    flux = matched_stars.flux[no_nan_indices],
+                    img_star_altaz = matched_stars.img_star_altaz[no_nan_indices],
+                    img_star_airmass = matched_stars.img_star_airmass[no_nan_indices]
+                    )
             try:
                 len(matched_stars.img_instr_mag)
             except TypeError:
                 print("Only 1 reference star detected in the image.")
                 continue
-            _, _, _, colour_index = astro.get_app_mag_and_index(matched_stars.ref_star, instr_filter)
-            field = astro.get_field_name(matched_stars, name_key='Name')
             # print(field)
             c_fci, zprime_f = astro.ground_based_first_order_transforms(matched_stars, instr_filter)
             gb_transform_table_columns = astro.update_gb_transform_table_columns(gb_transform_table_columns,
@@ -80,27 +95,27 @@ for dirpath, dirnames, filenames in os.walk(directory):
                                                                                  colour_index,
                                                                                  altazpositions)
             
-            large_table_columns = astro.update_large_table_columns(large_table_columns, 
-                                                                    matched_stars, 
-                                                                    hdr, 
-                                                                    exptime, 
-                                                                    ground_based=ground_based, 
-                                                                    name_key='Name')
+            # large_table_columns = astro.update_large_table_columns(large_table_columns, 
+            #                                                         matched_stars, 
+            #                                                         hdr, 
+            #                                                         exptime, 
+            #                                                         ground_based=ground_based, 
+            #                                                         name_key='Name')
 
 
 gb_transform_table = astro.create_gb_transform_table(gb_transform_table_columns)
 gb_transform_table.pprint_all()
 
-# mask = gb_transform_table['X'] < 2.1
-mask = gb_transform_table['Field'] != '106'
+mask = gb_transform_table['X'] < 2.1
+# mask = gb_transform_table['Field'] != '106'
 gb_transform_table = gb_transform_table[mask]
 gb_transform_table.pprint_all()
 
 gb_final_transforms = astro.ground_based_second_order_transforms(gb_transform_table, plot_results=True)
 gb_final_transforms.pprint_all()
 
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+# import matplotlib.pyplot as plt
+# import matplotlib.cm as cm
 # unique_filters = unique(gb_transform_table, keys='filter')
 # for unique_filter_row in unique_filters:
 #     unique_filter = unique_filter_row['filter']
@@ -125,35 +140,35 @@ import matplotlib.cm as cm
 #     plt.show()
 #     plt.close()
 
-large_stars_table = astro.create_large_stars_table(large_table_columns, ground_based=ground_based)
-large_stars_table.pprint_all()
-unique_fields = unique(large_stars_table, keys='Field')
-for unique_field in unique_fields['Field']:
-    if unique_field in list(gb_transform_table['Field']):
-        field_mask = large_stars_table['Field'] == unique_field
-        current_field = large_stars_table[field_mask]
-        unique_filters = unique(current_field, keys='filter')
-        for unique_filter in unique_filters['filter']:
-            filter_mask = current_field['filter'] == unique_filter
-            current_filter = current_field[filter_mask]
-            unique_stars = unique(current_filter, keys='Name')
-            colors = cm.rainbow(np.linspace(0, 1, len(unique_stars)))
-            X_plot = np.arange(start=min(current_filter['X'])-0.02, stop=max(current_filter['X'])+0.02, step=0.01)
-            for i, unique_star in enumerate(unique_stars['Name']):
-                star_mask = current_filter['Name'] == unique_star
-                current_star = current_filter[star_mask]
-                m, b = np.polyfit(current_star['X'], current_star['mag_instrumental'], 1)
-                plt.scatter(current_star['X'], current_star['mag_instrumental'], color=colors[i], label=unique_star)
-                plt.plot(X_plot, m*X_plot+b, color=colors[i])
-            # plt.plot(current_filter['X'], current_filter['mag_instrumental'], 'o')
-            plt.xlabel('X')
-            plt.ylabel(unique_filter.lower())
-            plt.ylim([min(current_filter['mag_instrumental'])*1.05, max(current_filter['mag_instrumental'])*0.95])
-            plt.gca().invert_yaxis()
-            plt.title(unique_field)
-            # plt.legend()
-            plt.show()
-            plt.close()
+# large_stars_table = astro.create_large_stars_table(large_table_columns, ground_based=ground_based)
+# large_stars_table.pprint_all()
+# unique_fields = unique(large_stars_table, keys='Field')
+# for unique_field in unique_fields['Field']:
+#     if unique_field in list(gb_transform_table['Field']):
+#         field_mask = large_stars_table['Field'] == unique_field
+#         current_field = large_stars_table[field_mask]
+#         unique_filters = unique(current_field, keys='filter')
+#         for unique_filter in unique_filters['filter']:
+#             filter_mask = current_field['filter'] == unique_filter
+#             current_filter = current_field[filter_mask]
+#             unique_stars = unique(current_filter, keys='Name')
+#             colors = cm.rainbow(np.linspace(0, 1, len(unique_stars)))
+#             X_plot = np.arange(start=min(current_filter['X'])-0.02, stop=max(current_filter['X'])+0.02, step=0.01)
+#             for i, unique_star in enumerate(unique_stars['Name']):
+#                 star_mask = current_filter['Name'] == unique_star
+#                 current_star = current_filter[star_mask]
+#                 m, b = np.polyfit(current_star['X'], current_star['mag_instrumental'], 1)
+#                 plt.scatter(current_star['X'], current_star['mag_instrumental'], color=colors[i], label=unique_star)
+#                 plt.plot(X_plot, m*X_plot+b, color=colors[i])
+#             # plt.plot(current_filter['X'], current_filter['mag_instrumental'], 'o')
+#             plt.xlabel('X')
+#             plt.ylabel(unique_filter.lower())
+#             plt.ylim([min(current_filter['mag_instrumental'])*1.05, max(current_filter['mag_instrumental'])*0.95])
+#             plt.gca().invert_yaxis()
+#             plt.title(unique_field)
+#             # plt.legend()
+#             plt.show()
+#             plt.close()
 # stars_table = astro.group_each_star(large_stars_table, ground_based=ground_based)
 # stars_table.pprint_all()
 # transform_index_list = ['(B-V)', '(V-R)', '(V-I)']
