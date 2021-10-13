f82004b83cbe09e0ec0c08eb00f4058e9779f182
Jack Wawrow <46384597+jmwawrow@users.noreply.github.com>
2021-10-13 14:20:17 -0400

    Process new images
    Process a new NEOSSat Landolt field.

--- V1 AstroSolver/RunSBTransforms.py
+++ V1 AstroSolver/RunSBTransforms.py
@@ -20,27 +20,29 @@ subfolder_list = [
 
 
 ref_stars_file = r'C:\Users\jmwawrow\Documents\DRDC_Code\FITS Tutorial\Reference_stars_Apr29.txt'
+ref_stars_file = "C:\\Users\\jack.wawrow\\Documents\\GitHub\\Astro2\\Reference Star Files\\Reference_stars_Apr29.txt"
 
 plot_results = True
-save_plots = False
+save_plots = True
 file_suffix = "_clean.fits"
 exposure_key = 'AEXPTIME'
 name_key = 'Name'
 transform_index_list = ['(B-V)', '(V-R)', '(V-I)']
 
-for subfolder in subfolder_list:
-    unique_id = subfolder
-    directory = f'C:\\Users\\jmwawrow\\Documents\\DRDC_Code\\NEOSSat Landolt Stars\\{subfolder}'
-    save_loc = os.path.join(directory, 'Outputs')
-    
-    sb_final_transform_table = astro._main_sb_transform_calc(directory, 
-                                                             ref_stars_file, 
-                                                             plot_results=plot_results, 
-                                                             save_plots=save_plots,
-                                                             file_suffix=file_suffix, 
-                                                             exposure_key=exposure_key,  
-                                                             name_key=name_key,
-                                                             transform_index_list=transform_index_list,
-                                                             save_loc=save_loc,
-                                                             unique_id=unique_id)
-    sb_final_transform_table.pprint_all()
+# for subfolder in subfolder_list:
+unique_id = 'SA97'
+# directory = f'C:\\Users\\jmwawrow\\Documents\\DRDC_Code\\NEOSSat Landolt Stars\\{subfolder}'
+directory = 'Z:\\SA97-NEOSSat'
+save_loc = os.path.join(directory, 'Outputs')
+
+sb_final_transform_table = astro._main_sb_transform_calc(directory, 
+                                                         ref_stars_file, 
+                                                         plot_results=plot_results, 
+                                                         save_plots=save_plots,
+                                                         file_suffix=file_suffix, 
+                                                         exposure_key=exposure_key,  
+                                                         name_key=name_key,
+                                                         transform_index_list=transform_index_list,
+                                                         save_loc=save_loc,
+                                                         unique_id=unique_id)
+sb_final_transform_table.pprint_all()
