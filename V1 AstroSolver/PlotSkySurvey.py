f820b91bfb048515153b6dfdb4a7fc9945f48779
Wawrow <jmwawrow@my.yorku.ca>
2021-10-22 15:43:49 -0400

    Update PlotSkySurvey.py
    Add FWHM v time plot for the sky survey.

--- V1 AstroSolver/PlotSkySurvey.py
+++ V1 AstroSolver/PlotSkySurvey.py
@@ -12,8 +12,10 @@ import matplotlib
 from matplotlib import cm
 import os
 import numpy as np
+from astropy.time import Time
+import matplotlib.dates as mdates
 
-survey_file = r"C:\Users\jack.wawrow\Documents\Suffield\2021 10 19\Standards\No Flats\Plate Solved\corrected_lights\Outputs_SkySurvey\auxiliary_table.csv"
+survey_file = r"C:\Users\jack.wawrow\Documents\Suffield\2021 10 19\Standards\No Flats\Plate Solved\corrected_lights\Outputs_SkySurvey\auxiliary_table_AllStars_PinPointZp.csv"
 # survey_file = r"C:\Users\jack.wawrow\Documents\Suffield\2021 10 19\Sky Survey\Automated Pointing Run 004\Outputs_SkySurvey\auxiliary_table.csv"
 star_aux_table = ascii.read(survey_file)
 
@@ -43,5 +45,21 @@ rlabels = ax.get_ymajorticklabels()
 for label in rlabels:
 	label.set_color('black')
 # plt.savefig(r'C:\Users\jack.wawrow\Documents\Suffield\2021 10 19\Standards\No Flats\Plate Solved\corrected_lights\Outputs_SkySurvey\BSB_plot')
+plt.show()
+plt.close()
+
+fwhm = star_aux_table['FWHM']
+fwhm_sigma = star_aux_table['FWHM_sigma']
+times_list = np.array(star_aux_table['Time (JD)'])
+times_obj = Time(times_list, format='jd', scale='utc')
+times_datetime = times_obj.to_value('datetime')
+
+fig, ax = plt.subplots()
+_, _, bars = ax.errorbar(times_datetime, fwhm, yerr=fwhm_sigma, fmt='o', markersize=2, capsize=0, elinewidth=0.75)
+[bar.set_alpha(0.3) for bar in bars]
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+ax.set_ylabel('FWHM')
+ax.set_xlabel('Time (UTC)')
+plt.title('FWHM v. Time')
 plt.show()
 plt.close()
