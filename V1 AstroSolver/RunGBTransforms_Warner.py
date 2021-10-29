ba6617d1ebac831d6a4d791066a48c0ee09ddad3
Wawrow <jmwawrow@my.yorku.ca>
2021-10-29 12:02:14 -0400

    Process new data

--- V1 AstroSolver/RunGBTransforms_Warner.py
+++ V1 AstroSolver/RunGBTransforms_Warner.py
@@ -56,7 +56,7 @@ unique_id = subfolder
 # directory = f'F:\\Intelsat 10-02\\{subfolder}\\Stars\\corrected_lights'
 # directory = r'C:\Users\jmwawrow\Documents\DRDC_Code\Intelsat 10-02\2021-09-17\corrected_lights\Calculation'
 # directory = r'Z:\2021-04-21 - unprocessed\Stars\corrected_lights'
-directory = r'C:\Users\jack.wawrow\Documents\Suffield\2021 10 19\Standards\Test\corrected_lights'
+directory = r'C:\Users\jack.wawrow\Documents\Suffield\2021 10 21\2021 10 21 - Raw Landolts\Plate Solved'
 save_loc = os.path.join(directory, 'Outputs_Warner_FewerStars')
 # try:
 large_stars_table = astro._main_gb_transform_calc_Warner(directory, 
--- V1 AstroSolver/printfitsheader.py
+++ V1 AstroSolver/printfitsheader.py
@@ -9,9 +9,9 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-file = fits.open(r"C:\Users\jack.wawrow\Documents\Suffield\2021 10 21\2021 10 21 - Raw Landolts\corrected_lights\2021_10_21_2x2_Light_B_-0.30C_10.000secs_SA111_00004868.fits")
+file = fits.open(r"C:\Users\jack.wawrow\Documents\Suffield\2021 10 21\2021 10 21 - Automated Pointing Run\corrected_lights\2021_10_21_2x2_Light_G_-0.10C_5.000secs_Target_#18_00004990.fits")
 print(repr(file[0].header))
-plt.imshow(file[0].data, cmap='gray')#, norm=LogNorm())
+plt.imshow(file[0].data, cmap='gray', norm=LogNorm())
 plt.show(block=True)
 plt.close()
 file.close()
