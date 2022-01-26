5564012d0aac0dba1a07d2e19bf7a6cb10133d0c
Michael Stewart <mstewart2525@gmail.com>
2022-01-26 14:11:01 -0500

    Made Fixes to File_Suffix
    - Made File_Suffix a Tuple instead of going through a list
    - PEP8 changes

--- V1 AstroSolver/RunGBTransforms.py
+++ V1 AstroSolver/RunGBTransforms.py
@@ -21,24 +21,24 @@ remove_large_airmass = False
 # lat_key='OBSGEO-B'
 # lon_key='OBSGEO-L'
 # elev_key='OBSGEO-H'
-file_suffix=[".fits", ".fit", ".fts"]
-exposure_key='EXPTIME'
-lat_key='SITELAT'
-lon_key='SITELONG'
-elev_key='SITEELEV'
-name_key='Name'
+file_suffix = (".fits", ".fit", ".fts")
+exposure_key = 'EXPTIME'
+lat_key = 'SITELAT'
+lon_key = 'SITELONG'
+elev_key = 'SITEELEV'
+name_key = 'Name'
 
 save_loc = os.path.join(directory, 'Outputs')
-gb_final_transforms, auxiliary_data_table = astro._main_gb_transform_calc(directory, 
-                                                                          ref_stars_file, 
-                                                                          plot_results=plot_results, 
-                                                                          save_plots=save_plots, 
-                                                                          remove_large_airmass_bool=remove_large_airmass, 
-                                                                          file_suffix=file_suffix, 
-                                                                          exposure_key=exposure_key, 
-                                                                          lat_key=lat_key, 
-                                                                          lon_key=lon_key, 
-                                                                          elev_key=elev_key, 
+gb_final_transforms, auxiliary_data_table = astro._main_gb_transform_calc(directory,
+                                                                          ref_stars_file,
+                                                                          plot_results=plot_results,
+                                                                          save_plots=save_plots,
+                                                                          remove_large_airmass_bool=remove_large_airmass,
+                                                                          file_suffix=file_suffix,
+                                                                          exposure_key=exposure_key,
+                                                                          lat_key=lat_key,
+                                                                          lon_key=lon_key,
+                                                                          elev_key=elev_key,
                                                                           name_key=name_key,
                                                                           save_loc=save_loc)
 
