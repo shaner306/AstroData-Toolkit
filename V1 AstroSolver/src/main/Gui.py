31bdfd6164c17f9b85d9ef0027595f44290d5b77
Jack Wawrow <46384597+jmwawrow@users.noreply.github.com>
2022-05-25 11:14:29 -0400

    Update Gui.py
    Add dependencies to PATH.

--- V1 AstroSolver/src/main/Gui.py
+++ V1 AstroSolver/src/main/Gui.py
@@ -55,6 +55,14 @@ from pathlib import Path
 import PySimpleGUI as sg
 from astropy.nddata import CCDData
 
+import sys
+from os.path import dirname
+src_path = dirname(dirname(__file__))
+sys.path.append(os.path.join(src_path, 'general_tools'))
+sys.path.append(os.path.join(src_path, 'transforms'))
+sys.path.append(os.path.join(src_path, 'pinpointsolving'))
+sys.path.append(os.path.join(src_path, 'transforms', 'TrackRateModeTransforms'))
+
 import AstroFunctions as astro
 import Main
 import main_transforms
