4d21c0b59894728fb38e681b4be2518e76e57a5b
Shane Ryall <Shaner306@protonmail.com>
2022-06-06 17:04:26 -0400

    Update streak detection for sffield data test

--- V1 AstroSolver/src/tests_and_verifications/StreakDetection.py
+++ V1 AstroSolver/src/tests_and_verifications/StreakDetection.py
@@ -18,7 +18,8 @@ from photutils.segmentation import deblend_sources
 from photutils.segmentation import detect_sources
 from photutils.segmentation import detect_threshold
 from photutils.segmentation import make_source_mask
-streak =r'/Users/home/Sync/'
+streak1 =r'/Users/home/Sync/'
+streak= r'/Users/home/Downloads/2021 10 21 - ZWO with C8/Test'
 
 file_suffix = (".fits", ".fit", ".FIT")
 trm=0
@@ -27,6 +28,7 @@ for dirpath, dirnames, filenames in os.walk(streak):
         if (filename.endswith(file_suffix)):
             filepath = os.path.join(dirpath, filename)
             STARS = open(filepath + '.stars', "w")
+            AstrometryNetFile=open(filepath+'.txt', 'w')
 
             imagehdularray = fits.open(filepath)
             fitsdata = imagehdularray[0].data
@@ -39,38 +41,49 @@ for dirpath, dirnames, filenames in os.walk(streak):
                 # fitsdata = fitsdata - bkg.background
                 # threshold = detect_threshold(fitsdata, nsigma=3.0)
 
-                threshold = bkg.background + (2.0 * bkg.background_rms)
+                threshold = bkg.background + (1.5 * bkg.background_rms)
                 sigma = 2.5 * gaussian_fwhm_to_sigma  # FWHM = 3.
                 kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
                 convolved_data = convolve(fitsdata, kernel, normalize_kernel=True)
-                segm = detect_sources(fitsdata, threshold, npixels=5)
-                segm_deblend = deblend_sources(fitsdata, segm, npixels=5,
-                                               nlevels=32, contrast=0.001)
+                segm = detect_sources(fitsdata, threshold, npixels=20)
+                segm_deblend = deblend_sources(fitsdata, segm, npixels=20,
+                                               nlevels=50, contrast=0.0001)
+            else:
+                bkg = Background2D(fitsdata, (30, 30), filter_size=(3,3), sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
+                #fitsdata = fitsdata - bkg.background
+                #threshold = detect_threshold(fitsdata, nsigma=3.0)
 
-            bkg = Background2D(fitsdata, (30, 30), filter_size=(3,3), mask=mask, bkg_estimator=bkg_estimator)
-            #fitsdata = fitsdata - bkg.background
-            #threshold = detect_threshold(fitsdata, nsigma=3.0)
+                threshold = bkg.background + (2.5 * bkg.background_rms)
+                sigma = 2.5 * gaussian_fwhm_to_sigma  # FWHM = 3.
+                kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
+                convolved_data = convolve(fitsdata, kernel, normalize_kernel=True)
+                segm = detect_sources(convolved_data, threshold, npixels=5)
+                segm_deblend = deblend_sources(convolved_data, segm, npixels=5,
+                                           nlevels=32, contrast=0.001)
 
-            threshold = bkg.background + (2.0 * bkg.background_rms)
-            sigma = 2.5 * gaussian_fwhm_to_sigma  # FWHM = 3.
-            kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
-            convolved_data = convolve(fitsdata, kernel, normalize_kernel=True)
-            segm = detect_sources(convolved_data, threshold, npixels=5)
-            segm_deblend = deblend_sources(convolved_data, segm, npixels=5,
-                                       nlevels=32, contrast=0.001)
-
-            cat = SourceCatalog(fitsdata, segm_deblend, convolved_data=convolved_data)
+            cat = SourceCatalog(fitsdata, segm_deblend, convolved_data=None)
 
             tbl = cat.to_table()
+
 
             newCenY = list(tbl['ycentroid'])
             newCenX = list(tbl['xcentroid'])
             newflux = list(tbl['segment_flux'])
-            el= list(tbl['eccentricity'])
-            print(el)
+
 
             for i in range(len(newCenY)):
                 streak_line = '{:.4f} {:.4f} 10 10 100 {:5.0f} 0 0.00'.format(float(newCenX[i]), float(newCenY[i]), newflux[i])
                 STARS.write(streak_line + "\n")
 
+            tbl.sort('segment_flux', reverse=True)
+            newCenY = list(tbl['ycentroid'])
+            newCenX = list(tbl['xcentroid'])
+            newflux = list(tbl['segment_flux'])
+
+            for i in range(len(newCenY)):
+                streak_line2 = '{:.4f} {:.4f}'.format(float(newCenX[i]), float(newCenY[i]))
+                AstrometryNetFile.write(streak_line2 + "\n")
+
+
             STARS.close()
+            AstrometryNetFile.close()
