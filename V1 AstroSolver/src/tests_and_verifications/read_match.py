f68b61fb5efb6b35e0a5fa381cf260b9c962a4fb
Jack Wawrow <46384597+jmwawrow@users.noreply.github.com>
2022-07-25 12:33:26 -0400

    Update read_match.py
    -Read Gaia G, BP, and RP magnitudes
    -Calculate Johnson-Cousins VRI magnitudes from Gaia mags
    -Start exploring calculating Johnson-Cousins B magnitude

--- V1 AstroSolver/src/tests_and_verifications/read_match.py
+++ V1 AstroSolver/src/tests_and_verifications/read_match.py
@@ -16,10 +16,44 @@ import AstroFunctions as astro
 import StreakDetection as sd
 
 
-filepath = r'/media/jmwawrow/Data/DRDC Data/ORC GBO/Field 1/139/Test/2021_139_DESCENT_46927_B_BESSEL_SA26_SF1_00000356.fit.corr'
+filepath = r'D:\DRDC Data\ORC GBO\Field 1\139\Test\2021_139_DESCENT_46927_B_BESSEL_SA26_SF1_00000356.fit.corr'
 # filepath = r'/media/jmwawrow/Data/DRDC Data/2022_07_16_Kingston_Trial_Survey/Light/2022_07_16_LIGHT_05sec/2022_07_16_1x1_5.000secs_0.00C_Light_CLEAR_NoTarget_00002019.fits.corr'
-hdul = fits.open(filepath)
-hdr = hdul[1].header
-data = hdul[1].data
-print(repr(hdr))
-print(data[0][13])
+
+def calculate_zmag(filepath):
+    hdr, data = astro.read_fits_file(filepath, extension=1)
+    print(repr(hdr))
+    g_mag, bp_mag, rp_mag = read_gaia_mags(data)
+    B, V, R, I = convert_gaia_to_bvri(g_mag, bp_mag, rp_mag)
+    print(V)
+    print(R)
+    print(I)
+
+def read_gaia_mags(imgdata):
+    g_mag = np.empty(np.shape(imgdata)[0])
+    bp_mag = np.empty(np.shape(imgdata)[0])
+    rp_mag = np.empty(np.shape(imgdata)[0])
+    for i, star in enumerate(imgdata):
+        g_mag[i] = star[13]
+        bp_mag[i] = star[24]
+        rp_mag[i] = star[25]
+    return g_mag, bp_mag, rp_mag
+
+def convert_gaia_to_bvri(g_mag, bp_mag, rp_mag):
+    B = np.empty(len(g_mag))
+    V = np.empty(len(g_mag))
+    R = np.empty(len(g_mag))
+    I = np.empty(len(g_mag))
+    # Calculation from: https://arxiv.org/pdf/1804.09368.pdf
+    V = g_mag + 0.01760 + (0.01760 * (bp_mag - rp_mag)) + (0.1732 * ((bp_mag - rp_mag)**2))
+    R = g_mag + 0.003226 - (0.3833 * (bp_mag - rp_mag)) + (0.1345 * ((bp_mag - rp_mag)**2))
+    I = g_mag - 0.02085 - (0.7419 * (bp_mag - rp_mag)) + (0.09631 * ((bp_mag - rp_mag)**2))
+    for i in range(len(g_mag)):
+        g_v = g_mag[i] - V[i]
+        const = -0.02907 - g_v
+        coeff = [-0.001768, -0.2297, -0.02385, const]
+        b_v = np.roots(coeff)
+        print(b_v)
+        # B[i] = b_v + V[i]
+    return B, V, R, I
+
+calculate_zmag(filepath)
