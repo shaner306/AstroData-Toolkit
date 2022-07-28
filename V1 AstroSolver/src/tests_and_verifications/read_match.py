48b28444fa5a8974056b28da57d94acc04c3cca2
Jack Wawrow <46384597+jmwawrow@users.noreply.github.com>
2022-07-28 15:47:34 -0400

    Update read_match.py
    -Create table containing reference stars and magnitudes

--- V1 AstroSolver/src/tests_and_verifications/read_match.py
+++ V1 AstroSolver/src/tests_and_verifications/read_match.py
@@ -3,10 +3,12 @@ import sys
 from tqdm import tqdm
 from astropy.coordinates import SkyCoord
 import astropy.units as u
-from astropy.io import fits
+from astropy.io import fits, ascii
+from astropy.table import QTable
 from astropy.wcs import WCS
 from math import atan
 import numpy as np
+import warnings
 from os.path import dirname
 src_path = dirname(dirname(__file__))
 sys.path.append(os.path.join(src_path, 'general_tools'))
@@ -19,24 +21,29 @@ import StreakDetection as sd
 filepath = r'D:\DRDC Data\ORC GBO\Field 1\139\Test\2021_139_DESCENT_46927_B_BESSEL_SA26_SF1_00000356.fit.corr'
 # filepath = r'/media/jmwawrow/Data/DRDC Data/2022_07_16_Kingston_Trial_Survey/Light/2022_07_16_LIGHT_05sec/2022_07_16_1x1_5.000secs_0.00C_Light_CLEAR_NoTarget_00002019.fits.corr'
 
-def calculate_zmag(filepath):
+def calculate_zmag(filepath, save_table=False):
+    save_loc = f'{filepath}.csv'
     hdr, data = astro.read_fits_file(filepath, extension=1)
-    # print(repr(hdr))
-    g_mag, bp_mag, rp_mag = read_gaia_mags(data)
+    print(repr(hdr))
+    ra, dec, g_mag, bp_mag, rp_mag = read_gaia_mags(data)
     B, V, R, I = convert_gaia_to_bvri(g_mag, bp_mag, rp_mag)
-    print(V)
-    print(R)
-    print(I)
+    ref_mag_table = create_ref_table(ra, dec, g_mag, bp_mag, rp_mag, B, V, R, I, 
+                                     save_table=save_table, save_loc=save_loc)
+    ref_mag_table.pprint_all()
 
 def read_gaia_mags(imgdata):
+    ra = np.empty(np.shape(imgdata)[0])
+    dec = np.empty(np.shape(imgdata)[0])
     g_mag = np.empty(np.shape(imgdata)[0])
     bp_mag = np.empty(np.shape(imgdata)[0])
     rp_mag = np.empty(np.shape(imgdata)[0])
     for i, star in enumerate(imgdata):
+        ra[i] = star[11]
+        dec[i] = star[12]
         g_mag[i] = star[13]
         bp_mag[i] = star[24]
         rp_mag[i] = star[25]
-    return g_mag, bp_mag, rp_mag
+    return ra, dec, g_mag, bp_mag, rp_mag
 
 def convert_gaia_to_bvri(g_mag, bp_mag, rp_mag):
     B = np.empty(len(g_mag))
@@ -52,8 +59,25 @@ def convert_gaia_to_bvri(g_mag, bp_mag, rp_mag):
         const = -0.02907 - g_v
         coeff = [-0.001768, -0.2297, -0.02385, const]
         b_v = np.roots(coeff)
-        print(b_v)
+        # print(b_v)
         # B[i] = b_v + V[i]
+    B[:] = np.nan
     return B, V, R, I
 
-calculate_zmag(filepath)
+
+def create_ref_table(ra, dec, g_mag, bp_mag, rp_mag, B, V, R, I, save_table=False, save_loc=None):
+    ref_mag_table = QTable(
+            names=['RA', 'Dec', 'G', 'BP', 'RP', 'B', 'V', 'R', 'I'],
+            data=[ra, dec, g_mag, bp_mag, rp_mag, B, V, R, I],
+            units=[u.degree, u.degree, u.mag, u.mag, u.mag, u.mag, u.mag, u.mag, u.mag]
+        )
+    if save_table:
+        if save_loc is not None:
+            ascii.write(ref_mag_table, save_loc, 'csv')
+        else:
+            warnings.warn('save_loc must be provided if save_table is True.')
+    return ref_mag_table
+
+
+os.remove(f'{filepath}.csv')
+calculate_zmag(filepath, save_table=True)
