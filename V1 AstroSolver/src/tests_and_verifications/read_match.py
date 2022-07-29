0f5ad4cb5da4575f7f086b0456ce4cf0e0746e80
Jack Wawrow <46384597+jmwawrow@users.noreply.github.com>
2022-07-29 11:09:39 -0400

    Update read_match.py
    -Read fluxes from xylist file
    -Calculate zero point using the Gaia G magnitude

--- V1 AstroSolver/src/tests_and_verifications/read_match.py
+++ V1 AstroSolver/src/tests_and_verifications/read_match.py
@@ -18,21 +18,26 @@ import AstroFunctions as astro
 import StreakDetection as sd
 
 
-filepath = r'D:\DRDC Data\ORC GBO\Field 1\139\Test\2021_139_DESCENT_46927_B_BESSEL_SA26_SF1_00000356.fit.corr'
+filepath = r'/media/jmwawrow/Data/DRDC Data/ORC GBO/Field 1/139/Test/2021_139_DESCENT_46927_B_BESSEL_SA38_00000406.fit'
 # filepath = r'/media/jmwawrow/Data/DRDC Data/2022_07_16_Kingston_Trial_Survey/Light/2022_07_16_LIGHT_05sec/2022_07_16_1x1_5.000secs_0.00C_Light_CLEAR_NoTarget_00002019.fits.corr'
 
-def calculate_zmag(filepath, save_table=False):
+def calculate_zmag(filepath, save_table=False, exposure_key='EXPTIME'):
     save_loc = f'{filepath}.csv'
-    hdr, data = astro.read_fits_file(filepath, extension=1)
-    print(repr(hdr))
+    hdr, _ = astro.read_fits_file(filepath)
+    _, data = astro.read_fits_file(f'{filepath}.corr', extension=1)
+    # print(repr(hdr))
     field_id, ra, dec, g_mag, bp_mag, rp_mag = read_gaia_mags(data)
     B, V, R, I = convert_gaia_to_bvri(g_mag, bp_mag, rp_mag)
     ref_mag_table = create_ref_table(field_id, ra, dec, g_mag, bp_mag, rp_mag, B, V, R, I, 
                                      save_table=save_table, save_loc=save_loc)
-    ref_mag_table.pprint_all()
-    # Read either the txt or xylist file here (hopefully with fluxes).
-    # Convert them to magnitudes.
-    # Use the field ids to find the index in the xylist.
+    # ref_mag_table.pprint_all()
+    instr_mags = read_field_mags(filepath, hdr, exposure_key)
+    # print(instr_mags)
+    mag_diff = calculate_mag_diff(hdr, ref_mag_table, field_id, instr_mags)
+    # print(mag_diff)
+    zmag, zmag_sigma = average_mag_diff(mag_diff)
+    print(f'Pinpoint zmag: {hdr["zmag"]}')
+    print(f'Zmag: {zmag:.3f} +/- {zmag_sigma:.3f}')
 
 def read_gaia_mags(imgdata):
     field_id = np.empty(np.shape(imgdata)[0], dtype=int)
@@ -79,13 +84,51 @@ def create_ref_table(field_id, ra, dec, g_mag, bp_mag, rp_mag, B, V, R, I, save_
         )
     if save_table:
         if save_loc is not None:
-            ascii.write(ref_mag_table, save_loc, 'csv')
+            ascii.write(ref_mag_table, save_loc, 'csv', overwrite=True)
         else:
             warnings.warn('save_loc must be provided if save_table is True.')
     return ref_mag_table
+
+
+def read_field_mags(filepath, hdr, exposure_key='EXPTIME'):
+    _, xyls = astro.read_fits_file(f'{filepath}.xyls', extension=1)
+    fluxes = np.empty(len(xyls))
+    for i, star in enumerate(xyls):
+        fluxes[i] = star[2]
+    exptime = hdr[exposure_key]
+    instr_mags = astro.calculate_magnitudes(fluxes, exptime)
+    return instr_mags
+
+
+def calculate_mag_diff(hdr, ref_mag_table, field_id, instr_mags):
+    corr_field_stars = instr_mags[field_id]
+    # Read the filter here and use that filter's ref mag.
+    indx_mags = np.array(ref_mag_table['G'])
+    mag_diff = indx_mags - corr_field_stars
+    return mag_diff
+
+
+def average_mag_diff(mag_diff):
+    zmag = np.mean(mag_diff)
+    zmag_sigma = np.std(mag_diff)
+    return zmag, zmag_sigma
 
 try:
     os.remove(f'{filepath}.csv')
 except FileNotFoundError:
     pass
-calculate_zmag(filepath, save_table=True)
+
+directory = r'/media/jmwawrow/Data/DRDC Data/ORC GBO/Field 1/139/Test/'
+file_suffix = '.fit'
+filecount = 0
+file_paths = []
+file_names = []
+for dirpth, _, files in os.walk(directory):
+    for file in files:
+        if file.endswith(file_suffix):
+            file_paths.append(os.path.join(dirpth, file))
+            file_names.append(file)
+            filecount += 1
+
+for file_path in file_paths:
+    calculate_zmag(file_path, save_table=True)
